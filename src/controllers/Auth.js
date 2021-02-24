c8d4b1e1d546c53406c3108af6bc71445ca9b02f
Karl Musingo <karlemusingo@gmail.com>
2021-02-24 21:58:48 +0200

    add reset password functionality

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { hashSync, compareSync } from 'bcrypt';
-import { sign, verify, decode } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 import statusCodes from '../constants/statusCodes';
 import jsonResponse from '../helpers/jsonResponse';
@@ -14,6 +14,8 @@ class AuthCtrl {
         Number(process.env.BCRYPT_SALTING_ROUNDS),
       ),
     });
+
+    // Send a message to the new phone number with 6 digits
 
     return jsonResponse({
       res,
@@ -65,6 +67,68 @@ class AuthCtrl {
       status: statusCodes.NOT_FOUND,
       res,
       message: 'Numéro de téléphone ou mot de passe incorrect',
+    });
+  }
+
+  static async initiateResetPassword(req, res) {
+    const existingRecord = await User.findOne({
+      phone: req.body.phone,
+    });
+
+    if (!existingRecord) {
+      return jsonResponse({
+        status: statusCodes.NOT_FOUND,
+        res,
+        message: `L'utilisateur avec ce numéro de téléphone (${req.body.phone}) n'a pas été trouvé`,
+      });
+    }
+
+    // on doit remplacer le token par un numero a 6 caracteres
+    const token = sign(
+      {
+        id: existingRecord._id,
+        phone: existingRecord.phone,
+        tokenType: 'passwordReset',
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '3h' },
+    );
+
+    // send sms with a random 6 digits
+
+    existingRecord.resetToken = token;
+    existingRecord.save();
+
+    return jsonResponse({
+      status: statusCodes.OK,
+      res,
+      message: 'Un sms de confirmation a été envoyé à votre numéro',
+    });
+  }
+
+  static async completeResetPassword(req, res) {
+    const user = await User.findOne({
+      resetToken: req.body.token,
+    });
+
+    if (user) {
+      user.password = hashSync(
+        req.body.password,
+        Number(process.env.BCRYPT_SALTING_ROUNDS),
+      );
+      user.resetToken = null;
+      user.save();
+
+      return jsonResponse({
+        status: statusCodes.OK,
+        res,
+        message: 'Le mot de passe a été restoré avec succès',
+      });
+    }
+    return jsonResponse({
+      status: statusCodes.NOT_ACCEPTABLE,
+      res,
+      message: "Le code de verification n'est pas correct",
     });
   }
 }
